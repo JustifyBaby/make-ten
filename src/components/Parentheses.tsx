b6c2dc987afdf8691c8c2533fc46598f2c8cf084
JustifyBaby <quronoa1232@gmail.com>
2024-12-27 15:17:34 +0900

    'terminal'

--- src/components/Parentheses.tsx
+++ src/components/Parentheses.tsx
@@ -14,15 +14,16 @@ const Parentheses = ({
   setNumList: Dispatch<SetStateAction<NumElement[]>>;
   isOpen: boolean;
 }) => {
-  const handleParenthesesChange = (id: number, isOpen: boolean) => {
-    const updatedData = [...numList];
-    const data = updatedData.find((item) => item.id === id);
-    if (isOpen) data!.openParentheses = !data!.openParentheses;
-    else data!.closeParentheses = !data!.closeParentheses;
+  // const handleParenthesesChange = (id: number, isOpen: boolean) => {z
+  //   const updatedData = [...numList];
+  //   const data = updatedData.find((item) => item.id === id);
+  //   if (isOpen) data!.openParentheses = !data!.openParentheses;
+  //   else data!.closeParentheses = !data!.closeParentheses;
 
-    setNumList(updatedData);
-  };
+  //   setNumList(updatedData);
+  // };
 
+  console.log(id, isOpen, current, numList, setNumList);
   return (
     <></>
     // <button
