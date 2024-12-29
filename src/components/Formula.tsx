140e446fb974d21ecdcefb5be2085a0522966da2
JustifyBaby <quronoa1232@gmail.com>
2024-12-29 23:34:00 +0900

    {$npm_config_m}

--- src/components/Formula.tsx
+++ src/components/Formula.tsx
@@ -188,7 +188,7 @@ export default function Formula() {
           } ${status === "error" && "text-red-600"}`}>
           {isRight}
         </div>
-        <div className='text-center font-medium'>{currentFormula}</div>
+        <div className='text-center font-medium p-2'>{currentFormula}</div>
         <div className='text-lg'>現在のスコア: {score}</div>
       </div>
     </main>
